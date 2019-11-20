08af664bf2801a4cbfbb1d7b46fa255b4f3967ca
Marina <enchanted_night@mail.ru>
2019-11-20 16:09:38 +0700

    Защита-2edits

--- js/backend.js
+++ js/backend.js
@@ -2,7 +2,7 @@
 
 (function () {
 
-  var URL_LOAD = 'https://jjs.dump.academy/keksobooking/data';
+  var URL_LOAD = 'https://js.dump.academy/keksobooking/data';
   var URL_SAVE = 'https://js.dump.academy/keksobooking';
   var TIMEOUT = 10000;
   var SUCCESS_RESPONSE = 200;
